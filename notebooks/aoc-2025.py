49dbd593502681ac41778031a9fb11bbaa0bc182
Timothy Leung <timothyleung@gmail.com>
2025-12-02 21:22:56 +0800

    Add Advent of Code 2025 Day 2

--- notebooks/aoc-2025.py
+++ notebooks/aoc-2025.py
@@ -75,3 +75,58 @@ for line in puzzle_input.splitlines():
 
 print(zeros)
 
+
+# %% [markdown]
+# # Day 2
+#
+
+# %%
+puzzle_input = open("../data/aoc/2025/02.txt", mode="r").read()
+
+puzzle_example = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+
+# %%
+total = 0
+
+for line in puzzle_input.split(","):
+    start, end = line.split("-")
+    for i in range(int(start), int(end) + 1):
+        s = str(i)
+        idx = len(s) // 2
+        if len(s) % 2 == 0 and s[:idx] == s[idx:]:
+            total += i
+
+print(total)
+
+# %%
+total = 0
+
+for line in puzzle_input.split(","):
+    start, end = line.split("-")
+    for i in range(int(start), int(end) + 1):
+        s = str(i)
+        if len(s) > 1 and len(set(s)) == 1:
+            total += i
+        elif len(s) == 4:
+            if s[:2] == s[2:]:
+                total += i
+        elif len(s) == 6:
+            if s[:2] == s[2:4] == s[4:]:
+                total += i
+            elif s[:3] == s[3:]:
+                total += i
+        elif len(s) == 8:
+            if s[:2] == s[2:4] == s[4:6] == s[6:]:
+                total += i
+            elif s[:4] == s[4:]:
+                total += i
+        elif len(s) == 9:
+            if s[:3] == s[3:6] == s[6:]:
+                total += i
+        elif len(s) == 10:
+            if s[:2] == s[2:4] == s[4:6] == s[6:8] == s[8:]:
+                total += i
+            elif s[:5] == s[5:]:
+                total += i
+
+print(total)
